bee58fc569d38144fd148ef732fee226e4ac3c3b
Partho P. Das <parthopdas@live.com>
2021-03-02 02:11:38 +1000

    Fixing up DAL for 1st cut wireup.

--- js_lib/pkg/pali_language_services_dal.js
+++ js_lib/pkg/pali_language_services_dal.js
@@ -1,3 +1,9 @@
-export const execSql = (sql) => `NYI - exec ${sql}`
+// NOTE: Ensure set window.__inflections_db before calling this function.
+const execSqlCore = (sql) => {
+    const results = window.__inflections_db.exec(sql)[0]
+    return results.values || []
+}
 
-export const execSqlWithTransliteration = (sql) => `NYI - exec with transliteration ${sql}`
+export const execSql = (sql) => JSON.stringify(execSqlCore(sql))
+
+export const execSqlWithTransliteration = (sql) => JSON.stringify(execSqlCore(sql))
