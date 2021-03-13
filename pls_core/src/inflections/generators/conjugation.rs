e2b5bb424d7ac3f193720e01029dbb250ab640e8
Partho P. Das <parthopdas@live.com>
2021-03-13 22:42:28 +1000

    Removing empty columns for conjugations

--- pls_core/src/inflections/generators/conjugation.rs
+++ pls_core/src/inflections/generators/conjugation.rs
@@ -20,6 +20,7 @@ lazy_static! {
 struct TenseViewModel {
     name: String,
     inflections_list: Vec<Vec<String>>,
+    ar_values_exist: Vec<bool>,
 }
 
 #[derive(Serialize)]
@@ -46,34 +47,72 @@ pub fn create_html_body(
         .map_err(|e| e.to_string())
 }
 
-fn create_tense_view_models(
-    table_name: &str,
-    transliterate: fn(&str) -> Result<String, String>,
+fn query_has_no_results(
+    query: &str,
     exec_sql: impl Fn(&str) -> Result<Vec<Vec<Vec<String>>>, String>,
-    stem: &str,
-) -> Result<Vec<TenseViewModel>, String> {
+) -> Result<bool, String> {
+    let count = &exec_sql(&query)?[0][0][0];
+    Ok(count.eq("0"))
+}
+
+struct ParameterValues {
+    pub t_values: Vec<String>,
+    pub p_values: Vec<String>,
+    pub ar_values: Vec<String>,
+    pub n_values: Vec<String>,
+}
+
+fn query_parameter_values(
+    exec_sql: impl Fn(&str) -> Result<Vec<Vec<Vec<String>>>, String>,
+) -> Result<ParameterValues, String> {
     let sql = r#"
         select * from _tense_values where name <> "";
         select * from _person_values where name <> "";
         select * from _actreflx_values where name <> "";
         select * from _number_values where name <> "" and name <> "dual";
     "#;
-    let values = exec_sql(sql)?;
+
+    let mut values = exec_sql(sql)?;
+    Ok(ParameterValues {
+        t_values: values.remove(0).into_iter().flatten().collect(),
+        p_values: values.remove(0).into_iter().flatten().collect(),
+        ar_values: values.remove(0).into_iter().flatten().collect(),
+        n_values: values.remove(0).into_iter().flatten().collect(),
+    })
+}
+
+fn create_tense_view_models(
+    table_name: &str,
+    transliterate: fn(&str) -> Result<String, String>,
+    exec_sql: impl Fn(&str) -> Result<Vec<Vec<Vec<String>>>, String>,
+    stem: &str,
+) -> Result<Vec<TenseViewModel>, String> {
+    let pvs = query_parameter_values(&exec_sql)?;
+
     let mut view_models: Vec<TenseViewModel> = Vec::new();
-    for t in values[0].iter().flatten() {
-        let count_sql = format!(
-            r#"select cast(count(*) as text) from {} where tense = "{}""#,
-            table_name, t
-        );
-        let count = &exec_sql(&count_sql)?[0][0][0];
-        if count.eq("0") {
+    for t in &pvs.t_values {
+        if query_has_no_results(
+            &format!(
+                r#"select cast(count(*) as text) from {} where tense = "{}""#,
+                table_name, t
+            ),
+            &exec_sql,
+        )? {
             continue;
         }
 
+        let mut ar_values_exist: Vec<bool> = Vec::new();
+        for ar in &pvs.ar_values {
+            ar_values_exist.push(!query_has_no_results(
+                &format!(r#"select cast(count(*) as text) from '{}' where tense = "{}" and actreflx = "{}""#, table_name, t, ar),
+                &exec_sql,
+            )?);
+        }
+
         let mut inflections_list: Vec<Vec<String>> = Vec::new();
-        for p in values[1].iter().flatten() {
-            for ar in values[2].iter().flatten() {
-                for n in values[3].iter().flatten() {
+        for p in &pvs.p_values {
+            for ar in &pvs.ar_values {
+                for n in &pvs.n_values {
                     let sql = format!(
                         r#"SELECT inflections FROM '{}' WHERE tense = '{}' AND person = '{}' AND actreflx = '{}' AND "number" = '{}'"#,
                         table_name, t, p, ar, n,
@@ -88,6 +127,7 @@ fn create_tense_view_models(
         let view_model = TenseViewModel {
             name: t.to_owned(),
             inflections_list,
+            ar_values_exist,
         };
         view_models.push(view_model);
     }
