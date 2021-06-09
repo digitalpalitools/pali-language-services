15470c52c3a231dbed4919d672a96f0f940bec03
Catalin Iosif <ciosif@axway.com>
2021-06-09 12:07:35 +0300

    Fix for web-ui #30 and #31 - add pos and meaning to stems and indeclinables

--- pls_core/src/inflections/generators/indeclinable.rs
+++ pls_core/src/inflections/generators/indeclinable.rs
@@ -1,4 +1,4 @@
-use crate::inflections::{localise_abbrev, PlsInflectionsHost};
+use crate::inflections::{localise_abbrev, PlsInflectionsHost, pmd::get_pali1_metadata};
 use tera::{Context, Tera};
 
 lazy_static! {
@@ -21,9 +21,12 @@ pub fn create_html_body(
     host: &dyn PlsInflectionsHost,
 ) -> Result<(String, bool), String> {
     let mut context = Context::new();
+    let pm = get_pali1_metadata(&word, host)?;
 
     context.insert("word", &host.transliterate(word)?);
     context.insert("is_inflected_form", &is_inflected_form);
+    context.insert("meaning", &pm.meaning);
+    context.insert("pos", &pm.pos);
     let body = TEMPLATES
         .render("indeclinable", &context)
         .map_err(|e| e.to_string())?;
